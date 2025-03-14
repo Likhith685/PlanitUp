937de555e4364d57559561dad0192770509276ac
TSG7 <tsg0704@gmail.com>
2025-03-14 22:24:06 +0530

    about us

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import AboutUs from './components/aboutus/About'
 
 const App = () => {
   return (
     <div>
         <Routes>
           <Route path='/' element={<Home/>}> </Route>
+          <Route path='/about' element={<AboutUs/>}> </Route>
        </Routes>
     </div>
   )
